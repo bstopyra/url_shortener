80f435bc2f78c2be3ab47f88632b92d7b18ecc38
ogierhaq <ogierhaq@gmail.com>
2021-10-04 15:48:43 +0200

    fixed a typo

--- frontend/src/components/URLShortener.js
+++ frontend/src/components/URLShortener.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./URLShortener.css";
 import { Button, Container, Row, Col, Form, Alert } from "react-bootstrap";
 
 const endpoint = "http://127.0.0.1:8080";
